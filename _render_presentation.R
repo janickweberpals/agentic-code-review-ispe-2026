0e2f8232430a9023f58bb4e7d7f52b9d45cbac8b
Janick Weberpals, RPh, PhD <83728221+janickweberpals@users.noreply.github.com>
2026-06-15 11:49:01 +0200

    fix: fixed bad merge

--- _render_presentation.R
+++ _render_presentation.R
@@ -1,18 +1,13 @@
-system("quarto render code_review_presentation.qmd")
-file.copy(
-  "code_review_presentation.html",
-  "_book/code_review_presentation.html",
-  overwrite = TRUE
-)
-
-# scripts to run before rendering the website
-
-# Load required packages
 library(quarto)
 
-# render Introduction to ICPE 2025 course
 quarto::quarto_render(
-  input = "code_review_presentation.qmd",
+  input         = "code_review_presentation.qmd",
   output_format = "revealjs",
-  output_file = "code_review_presentation.html"
+  output_file   = "code_review_presentation.html"
+)
+
+file.copy(
+  from      = "code_review_presentation.html",
+  to        = "_book/code_review_presentation.html",
+  overwrite = TRUE
 )
